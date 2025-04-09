2f61035cc88acb85fbd4ca345570de150af4fcd2
braniubojni <erik95harutyunyan@gmail.com>
2025-04-09 18:54:33 +0200

    Add test suite for account operations and utility functions

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:4d45b61f-5df9-4568-a912-d89d2e7c13a2@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db: ", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
